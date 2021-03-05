f95f2526586476ae555cd93164da35cd64237c7f
takumi-hiraoka <takumi-hiraoka@takumi-hiraokanoMacBook-Air.local>
2021-03-05 12:52:13 +0900

    first commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let a;
+a = { b: 1 };
+a = { b: 1, c: undefined };
+a = { b: 1, c: 'd' };
+a = { b: 1, 10: true };
+a = { b: 1, 10: true, 20: false };
+console.log(a);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let a: {
+  b: number,
+  c?: string
+  [key: number]: boolean
+}
+a = {b: 1}
+a = {b: 1, c: undefined}
+a = {b: 1, c: 'd'}
+a = {b: 1, 10: true}
+a = {b: 1, 10: true, 20: false}
+
+console.log(a);
